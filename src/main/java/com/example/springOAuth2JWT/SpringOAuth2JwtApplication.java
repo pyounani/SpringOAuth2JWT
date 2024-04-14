33fdf4453f85efb05a4d1c12024e59961d7b0206
pyounani <yndbsk9372@gmail.com>
2024-04-14 16:45:41 +0900

    first commit

---
+++ src/main/java/com/example/springOAuth2JWT/SpringOAuth2JwtApplication.java
@@ -0,0 +1,13 @@
+package com.example.springOAuth2JWT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOAuth2JwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringOAuth2JwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springOAuth2JWT/SpringOAuth2JwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springOAuth2JWT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOAuth2JwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
